17d3d823f394aceaa41cbd385b512032b216046b
Turtvaiz <starcomet82@gmail.com>
2020-04-26 21:31:59 +0300

    Misc. fixes. See description
    Wrapped the whole main script in a while True wrapper and a try to avoid crashes from servers not responding. Also fixed searching with tags that had escaped formatting with backslashes (\). Also cleaned up a bit.

--- bot.py
+++ bot.py
@@ -4,6 +4,10 @@ import json
 import time
 import re
 import requests
+import logging
+
+# to fix ugly multiline strings
+from textwrap import dedent
 
 
 # open file in current working directory, so hopefully the same as the script's directory
@@ -40,109 +44,151 @@ def add_comment_id(id):
         f.write(f"{id}\n")
 
 
-def search_url(tags):
-    # blacklist = []
-    base_url = "https://e621.net/posts.json?tags=order%3Arandom+score%3A>25"
-    url = base_url + "+" + "+".join(tags)
-
-
-print("bot started")
-# start listening for new comments
-for comment in subreddit.stream.comments(skip_existing=True):
-    print(f"processing #{comment.id}")
-    # if id is not new, skip it. Technically this shouldn't be necessary since we're skipping existing, but whatever
-    if (
-        check_comment_id(comment.id)
-        or comment.author.name.lower() == file_info[3].lower()
-    ):
-        continue
-    else:
-        add_comment_id(comment.id)
-
-    # take comment text and split into lines and turn into lowercase
-    text_lines = [x.lower() for x in comment.body.split("\n")]
-
-    # then check if there's actually a command
-    if all(["furbot search" not in line for line in text_lines]):
-        continue
-
-    for line in text_lines:
-        # find earlier search command and get the tags
-        regex = re.search(r"furbot search (.+)", line)
-        # we don't need multiple matches so break out
-        if regex:
-            regex_result = regex.group(1)
-            break
-
-    # parse tags into list
-    search_tags = regex_result.split(" ")
-
-    # make a search link out of them, and fetch the result_json
-    base_link = "https://e621.net/posts.json?tags=order%3Arandom+score%3A>25+-gore+-castration+-feces+-scat+-hard_vore"
-    result_json = requests.get(
-        base_link + "+" + "+".join(search_tags), headers=header
-    ).text
-
-    # parse the response json into a list of dicts, where each post is a dict
-    posts = list(json.loads(result_json)["posts"])
-
-    # create the Post | Direct link text
-    if len(posts) == 0:
-        link_text = "No results found. You may have an invalid tag, or all results have a score below 25."
-        tag_list = []
-    else:
-        # select first post
-        first_post = posts[0]
-
-        # Find url of first post. Oddly everything else has a cool direct link into it, but the json only supplies the id of the post and not the link.
-        page_url = "https://e621.net/posts/" + str(first_post["id"])
-
-        # Tags are separated into general species etc so combine them into one.
-        tag_list = (
-            first_post["tags"]["general"]
-            + first_post["tags"]["species"]
-            + first_post["tags"]["character"]
-            + first_post["tags"]["copyright"]
-            + first_post["tags"]["artist"]
-            + first_post["tags"]["invalid"]
-            + first_post["tags"]["lore"]
-            + first_post["tags"]["meta"]
-        )
-
-        # Check for swf/flash first before setting direct link to full image.
-        if first_post["file"]["ext"] == "swf":
-            direct_link = "Direct links do not work properly with flash animations. Please check the post."
+def main():
+    # start listening for new comments
+    for comment in subreddit.stream.comments():
+        # if id is not new, or the author has the same name as the bot's user, skip it.
+        if (
+            check_comment_id(comment.id)
+            or comment.author.name.lower() == file_info[3].lower()
+        ):
+            add_comment_id()
+            continue
         else:
-            # for some reason putting the thing below straight into the f-string is invalid syntax?
-            dlink = first_post["file"]["url"]
-            direct_link = f"[Direct Link]({dlink})"
-        link_text = f"[Post]({page_url}) | {direct_link}"
+            print(f"processing #{comment.id}")
 
-    if len(tag_list) == 0:
-        tags_message = ""
-    else:
-        tags_message = f"\n\n**^^^^Post ^^^^Tags:** ^^^^{' ^^^^'.join(tag_list)}\n\n"
+        # remove backslashes, since occasionally they will mess up searches with
+        # escaped underscores: e.g. long\_tag\_thing
+        comment_body = comment.body.replace("\\", "")
 
-    # compose the final message.
-    # yes multiline strings are very ugly, and I don't want to clean it up yet
-    message_body = f"""
-Hello, {comment.author.name}. Here are the results for your search:
+        # take comment text and split into lines and turn into lowercase
+        text_lines = [x.lower() for x in comment.body.split("\n")]
 
-{" ".join(search_tags)}
+        # then check if there's actually a command
+        if all(["furbot search" not in line for line in text_lines]):
+            continue
 
-{link_text}{tags_message}
+        for line in text_lines:
+            # find earlier search command and get the tags
+            regex = re.search(r"furbot search (.+)", line)
+            # we don't need multiple matches so break out
+            if regex:
+                regex_result = regex.group(1)
+                break
 
----
+        # parse tags into list
+        search_tags = regex_result.split(" ")
+
+        # make a search link out of them, and fetch the result_json
+        base_link = "https://e621.net/posts.json?tags=order%3Arandom+score%3A>19+-gore+-castration+-feces+-scat+-hard_vore"
+        r = requests.get(base_link + "+" + "+".join(search_tags), headers=header)
+        r.raise_for_status()
+        result_json = r.text
+
+        # parse the response json into a list of dicts, where each post is a dict
+        posts = list(json.loads(result_json)["posts"])
+
+        # create the Post | Direct link text
+        if len(posts) == 0:
+            # test if score was the problem by requesting another list from the site,
+            # but wait for a second to definitely not hit the limit rate
+            time.sleep(1)
+            unscored_base_link = "https://e621.net/posts.json?tags=order%3Arandom+-gore+-castration+-feces+-scat+-hard_vore"
+
+            # then we can make a link text without score limit
+            r = requests.get(
+                unscored_base_link + "+" + "+".join(search_tags), headers=header
+            )
+            r.raise_for_status()
+            unscored_result_json = r.text
+
+            # which we use to explain why there were no results,
+            # since the bot can sometimes be confusing to use
+            if len(list(json.loads(unscored_result_json)["posts"])) == 0:
+                link_text = "No results found. You may have an invalid tag, or all possible results had blacklisted tags."
+            else:
+                link_text = "No results found. All results had a score below 20."
+            tag_list = []
+        else:
+            # select first post
+            first_post = posts[0]
+
+            # Find url of first post. Oddly everything else has a cool direct link into it, but the json only supplies the id of the post and not the link.
+            page_url = "https://e621.net/posts/" + str(first_post["id"])
+
+            # Tags are separated into general species etc so combine them into one.
+            tag_list = (
+                first_post["tags"]["general"]
+                + first_post["tags"]["species"]
+                + first_post["tags"]["character"]
+                + first_post["tags"]["copyright"]
+                + first_post["tags"]["artist"]
+                + first_post["tags"]["invalid"]
+                + first_post["tags"]["lore"]
+                + first_post["tags"]["meta"]
+            )
+
+            # Check for swf/flash first before setting direct link to full image.
+            if first_post["file"]["ext"] == "swf":
+                direct_link = "Direct links do not work properly with flash animations. Please check the post."
+            else:
+                # for some reason putting the thing below straight into the f-string is invalid syntax?
+                dlink = first_post["file"]["url"]
+                direct_link = f"[Direct Link]({dlink})"
+            link_text = f"[Post]({page_url}) | {direct_link}"
+
+        if len(tag_list) == 0:
+            tags_message = ""
+        else:
+            tags_message = (
+                f"\n\n**^^^^Post ^^^^Tags:** ^^^^{' ^^^^'.join(tag_list)}\n\n"
+            )
+
+        # compose the final message.
+        message_body = f"""\
+                        Hello, {comment.author.name}. Here are the results for your search:
+
+                        {" ".join(search_tags)}
+
+                        {link_text}{tags_message}
+
+                        ---
+
+                        I am a bot and a quick and temporary replacement for the real and original furbot. Contact \/u\/heittoaway if this bot is going crazy or for more information.\
+                        """
+
+        print("replying...")
+        add_comment_id(comment.id)
+        # dedent fixes the indentations from above, so the message doesn't appear as a code block
+        comment.reply(dedent(message_body))
+        print("succesfully replied")
+
+        # this makes the bot wait after handling a new comment
+        # it should slow down any loops and nicely prevents the bot from exceeding e621's request limit rate
+        # it nicely also only works if a match was found so the bot **shouldn't** get stuck on other comments
+        time.sleep(5)
 
 
-I am a bot and a quick and temporary replacement for the real and original furbot. Contact \/u\/heittoaway if this bot is going crazy or for more information.
-"""
-
-    print("replying...")
-    comment.reply(message_body)
-    print(" succesful\n")
-
-    # this makes the bot wait after handling a new comment
-    # it should slow down any loops and nicely prevents the bot from exceeding e621's request limit rate
-    # it nicely also only works if a match was found so the bot **shouldn't** get stuck on other comments
-    time.sleep(5)
+print("Bot started")
+# since PRAW doesn't handle the usual 503 errors caused by reddit's awful servers,
+# they have to be handled manually. Additionally, whenever an error is raised, the
+# stream stops, so we need this ugly wrapper:
+while True:
+    try:
+        main()
+    except praw.exception.APIException:
+        print("Reddit most likely returned 503.")
+        print("Waiting for 60 seconds.")
+        time.sleep(60)
+    except requests.exceptions.HTTPError as e:
+        logging.exception("Caugh an HTTPError.")
+        print("Waiting for 60 seconds.")
+        time.sleep(60)
+    except requests.RequestException as e:
+        logging.exception("Caugh an exception from requests.")
+        print("Waiting for 60 seconds.")
+        time.sleep(60)
+    except Exception as e:
+        logging.exception("Caugh an unknown exception.")
+        print("Waiting for 120 seconds.")
+        time.sleep(120)
